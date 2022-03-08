150d425fd73584616d23112d3012337ad7f28998
Citem7 <45088194+Citem7@users.noreply.github.com>
2022-03-08 08:28:25 +0800

    Update Node.cs

--- Episode 03 - astar/Assets/Scripts/Node.cs
+++ Episode 03 - astar/Assets/Scripts/Node.cs
@@ -2,14 +2,18 @@
 using System.Collections;
 
 public class Node {
-	
+	//是否是障碍物
 	public bool walkable;
+	//世界坐标
 	public Vector3 worldPosition;
+	//网格坐标
 	public int gridX;
 	public int gridY;
-
+   	//离起点的距离，已经走得距离
 	public int gCost;
+	//离终点的距离，估计值
 	public int hCost;
+	//回溯路径，记录的父节点
 	public Node parent;
 	//构造函数
 	public Node(bool _walkable, Vector3 _worldPos, int _gridX, int _gridY) {
